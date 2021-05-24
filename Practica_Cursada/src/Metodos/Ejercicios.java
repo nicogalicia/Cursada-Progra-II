0b49c146786270fde7277bf2b7652f0748c2781d
nicogalicia <galiciamorenonicolas@gmail.com>
2021-05-24 18:16:19 -0300

    Update Ejercicios.java

--- Practica_Cursada/src/Metodos/Ejercicios.java
+++ Practica_Cursada/src/Metodos/Ejercicios.java
@@ -1,12 +1,23 @@
 package Metodos;
 
 import apis.PilaTDA;
+import impl.PilaTI;
 
 public class Ejercicios {
-    public static void pasarPila(PilaTDA p1, PilaTDA p2) {
+    public static void pasarPila(PilaTDA p1, PilaTDA p2) { //Pasa a otra pila, dejandola en orden inverso
         while (!p1.pilaVacia()) {
             p2.apilar(p1.tope());
             p1.desapilar();
+        }
+    }
+
+    public static void copiarPila(PilaTDA p1, PilaTDA p2) { //Copia la pila a otra manteniendo el orden
+        PilaTDA p3 = new PilaTI();
+        while (!p1.pilaVacia()) {
+            p3.apilar(p1.tope());
+            p1.desapilar();
+            p2.apilar(p3.tope());
+            p3.desapilar();
         }
     }
 }
