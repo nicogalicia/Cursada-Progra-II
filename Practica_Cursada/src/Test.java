9c4656251cd75eb4fa405163d1a80a397bd7c77b
nicogalicia <galiciamorenonicolas@gmail.com>
2021-05-26 07:43:34 -0300

    .

--- Practica_Cursada/src/Test.java
+++ Practica_Cursada/src/Test.java
@@ -1,7 +1,19 @@
-
+import apis.ColaTDA;
+import impl.ColaLD;
 
 public class Test {
     public static void main(String[] args) {
 
+        ColaTDA c1 = new ColaLD();
+        c1.inicializarCola();
+
+        for (int i = 0; i<=10; i++) {
+            c1.acolar(i);
+        }
+
+        while (!c1.colaVacia()) {
+            System.out.println(c1.primero());
+            c1.desacolar();
+        }
     }
 }
