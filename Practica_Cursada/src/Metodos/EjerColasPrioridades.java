e355bb8395272283cbbea650f38c12a1d2edfc58
nicogalicia <galiciamorenonicolas@gmail.com>
2021-05-31 20:33:47 -0300

    .

--- Practica_Cursada/src/Metodos/EjerColasPrioridades.java
+++ Practica_Cursada/src/Metodos/EjerColasPrioridades.java
@@ -3,6 +3,8 @@ package Metodos;
 import apis.ColaPrioridadTDA;
 import impl.ColaPrioridadAO;
 
+import java.util.concurrent.LinkedBlockingDeque;
+
 public class EjerColasPrioridades {
     public static void copiarColarPrioridad(ColaPrioridadTDA cp1, ColaPrioridadTDA cp2) {
         ColaPrioridadTDA colaAux = new ColaPrioridadAO();
@@ -34,6 +36,64 @@ public class EjerColasPrioridades {
     }
 
     public static void combinarColasPrioridades (ColaPrioridadTDA cp1, ColaPrioridadTDA cp2, ColaPrioridadTDA cp3) { //A igual prioridad, los de CP1 van primero
-        
+        ColaPrioridadTDA auxCola1 = new ColaPrioridadAO();
+        ColaPrioridadTDA auxCola2 = new ColaPrioridadAO();
+        auxCola1.inicializarCola();
+        auxCola2.inicializarCola();
+
+        copiarColarPrioridad(cp1, auxCola1);
+        copiarColarPrioridad(cp2, auxCola2);
+
+        while (!auxCola1.colaVacia()) {
+            cp3.acolarPrioridad(auxCola1.primero(), auxCola1.prioridad());
+            auxCola1.desacolar();
+        }
+
+        while (!auxCola2.colaVacia()) {
+            cp3.acolarPrioridad(auxCola2.primero(), auxCola2.prioridad());
+            auxCola2.desacolar();
+        }
+    }
+
+    public static int contarElementosColasPrioridades (ColaPrioridadTDA cp1) {
+        int cantidad;
+        ColaPrioridadTDA colaAux = new ColaPrioridadAO();
+        colaAux.inicializarCola();
+
+        copiarColarPrioridad(cp1, colaAux);
+
+        cantidad=0;
+        while (!colaAux.colaVacia()) {
+            cantidad++;
+            colaAux.desacolar();
+        }
+
+        return cantidad;
+    }
+
+    public static boolean determinarColaPrioridadIdenticas (ColaPrioridadTDA cp1, ColaPrioridadTDA cp2) {
+        boolean identicas = true;
+        ColaPrioridadTDA auxCola1 = new ColaPrioridadAO();
+        ColaPrioridadTDA auxCola2 = new ColaPrioridadAO();
+        auxCola1.inicializarCola();
+        auxCola2.inicializarCola();
+
+        copiarColarPrioridad(cp1, auxCola1);
+        copiarColarPrioridad(cp2, auxCola2);
+
+        if (contarElementosColasPrioridades(auxCola1) != contarElementosColasPrioridades(auxCola2)) {
+            identicas = false;
+        }
+        else {
+            while (!auxCola1.colaVacia()) {
+                if (auxCola1.primero() != auxCola2.primero() || auxCola1.prioridad() != auxCola2.prioridad()) {
+                    identicas = false;
+                }
+                auxCola1.desacolar();
+                auxCola2.desacolar();
+            }
+        }
+
+        return identicas;
     }
 }
--- Practica_Cursada/src/Test.java
+++ Practica_Cursada/src/Test.java
@@ -13,21 +13,21 @@ public class Test {
     public static void main(String[] args) {
         ColaPrioridadTDA cp1 = new ColaPrioridadAO();
         ColaPrioridadTDA cp2 = new ColaPrioridadAO();
+        ColaPrioridadTDA cp3 = new ColaPrioridadAO();
 
         cp1.inicializarCola();
         cp2.inicializarCola();
+        cp3.inicializarCola();
+
 
         int aux = 0;
-        for (int i = 0; i<=10; i++) {
+        for (int i = 1; i<=10; i++) {
             cp1.acolarPrioridad(i, aux);
-            aux++;
-        }
-
-        aux =0;
-        for (int i = 11; i<20; i++) {
             cp2.acolarPrioridad(i, aux);
             aux++;
         }
+
+
 
     }
 }
