7151c828ee1eda010f4a8b675d4a08cbd9592d4a
nicogalicia <galiciamorenonicolas@gmail.com>
2021-06-01 01:50:09 -0300

    .

---
+++ Practica_Cursada/src/Ejercicios_TP1/imprimirPila.java
@@ -0,0 +1,18 @@
+package Ejercicios_TP1;
+
+import Metodos.EjerPilas;
+import apis.PilaTDA;
+import impl.PilaTF;
+
+public class imprimirPila {
+    public static void main(String[] args) {
+        PilaTDA p1 = new PilaTF();
+        p1.inicializarPila();
+
+        for (int i = 0; i<=10; i++) {
+            p1.apilar(i);
+        }
+
+        EjerPilas.imprimirPila(p1);
+    }
+}
--- Practica_Cursada/src/Ejercicios_TP6/test.java
+++
@@ -1,4 +0,0 @@
-package Ejercicios_TP6;
-
-public class test {
-}
