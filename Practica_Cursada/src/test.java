0b559e06b847c85a5c938104534f6808b203b65d
nicogalicia <galiciamorenonicolas@gmail.com>
2021-06-05 18:55:26 -0300

    .

--- Practica_Cursada/src/test.java
+++ Practica_Cursada/src/test.java
@@ -1,13 +1,16 @@
 import Metodos.Auxiliares;
 import Metodos.EjerConjuntos;
 import Metodos.EjerPilas;
+import apis.ColaTDA;
 import apis.ConjuntoTDA;
 import apis.PilaTDA;
 import impl.ConjuntoLD;
 import impl.PilaTF;
+import impl.PilaTI;
 
 public class test {
     public static void main(String[] args) {
 
+        
     }
 }
