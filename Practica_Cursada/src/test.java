beced1c253f0fa3e590d9cc137cd46e2405d367a
nicogalicia <galiciamorenonicolas@gmail.com>
2021-06-10 17:40:22 -0300

    Update test.java

--- Practica_Cursada/src/test.java
+++ Practica_Cursada/src/test.java
@@ -1,15 +1,3 @@
-import Metodos.Auxiliares;
-import Metodos.EjerConjuntos;
-import Metodos.EjerPilas;
-import apis.ABBTDA;
-import apis.ColaTDA;
-import apis.ConjuntoTDA;
-import apis.PilaTDA;
-import impl.ABB;
-import impl.ConjuntoLD;
-import impl.PilaTF;
-import impl.PilaTI;
-
 public class test {
     public static void main(String[] args) {
 
