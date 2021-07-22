06a82ea172baaf8a360910a04cf8fb3b400e68ac
nicogalicia <galiciamorenonicolas@gmail.com>
2021-07-22 20:31:31 -0300

    .

--- Practica_Cursada/src/Metodos/EjerArboles.java
+++ Practica_Cursada/src/Metodos/EjerArboles.java
@@ -18,18 +18,22 @@ public class EjerArboles {
 
     }
 
-    public static boolean esHojaEnABB(ABBTDA a, int x) {
-        boolean retorno=false;
-        if (!a.arbolVacio()) {
-            if(a.raiz()==x && a.hijoIzq().arbolVacio() && a.hijoDer().arbolVacio())
-                retorno=true;
-            else
-            if (a.raiz() > x)
-                retorno=existeElementoEnABB(a.hijoIzq(),x);
-            else
-                retorno=existeElementoEnABB(a.hijoDer(),x);
+    public static boolean esHojaEnABB(ABBTDA arbol, int valorBuscado) {
+        if (arbol.arbolVacio()) {
+            return false; // no existen datos en el arbol
+        } else {
+            if (arbol.raiz() == valorBuscado) {
+                if (arbol.hijoIzq().arbolVacio() && arbol.hijoDer().arbolVacio())
+                    return true; // si el valor no posee valores hijos (no es raiz) retorna verdadero
+                else
+                    return false;
+            } else {
+                if (valorBuscado > arbol.raiz())
+                    return esHojaEnABB(arbol.hijoDer(), valorBuscado);
+                else
+                    return esHojaEnABB(arbol.hijoIzq(), valorBuscado);
+            }
         }
-        return retorno;
     }
     public static int calcularProfundidad(ABBTDA a, int x){
         int retorno=-1;
@@ -53,34 +57,28 @@ public class EjerArboles {
         return retorno;
     }
 
-    public static int sumar(ABBTDA a){
-        int retorno=0;
-        if (!a.arbolVacio())
-            retorno= a.raiz()+ cantidadElementos(a.hijoIzq())+ cantidadElementos(a.hijoDer()) ; // s=s+a
-        return retorno;
+    public static int sumar(ABBTDA arbol) {
+        if (arbol.arbolVacio())
+            return 0;
+        else {
+            return arbol.raiz() + sumar(arbol.hijoDer()) + sumar(arbol.hijoIzq());
+        }
     }
 
-    public static int contarHojas(ABBTDA a){
-        int retorno=0;
-        if (!a.arbolVacio())
-            retorno= (a.hijoDer().arbolVacio() && a.hijoIzq().arbolVacio())? 1:0+ cantidadElementos(a.hijoIzq())+ cantidadElementos(a.hijoDer()) ;
-        retorno--;
-        return retorno;
-    }
-
-
-    public static int contarHojas1(ABBTDA a){
-        int retorno=0;
-        if (!a.arbolVacio())
-            if(a.hijoDer().arbolVacio() && a.hijoIzq().arbolVacio())
-                retorno=1;
+    public static int contarHojas(ABBTDA arbol) {
+        if (arbol.arbolVacio())
+            return 0;
+        else {
+            if (arbol.hijoDer().arbolVacio() && arbol.hijoIzq().arbolVacio())
+                return 1;
             else
-                retorno= 0+ cantidadElementos(a.hijoIzq())+ cantidadElementos(a.hijoDer()) ;
-        retorno--;
-        return retorno;
+                return contarHojas(arbol.hijoDer()) + contarHojas(arbol.hijoIzq());
+        }
     }
+
 
     public static void preorder(ABBTDA a) {
+        //Imprime raiz, después arboles menores y después mayores
         if (!a.arbolVacio()) {
             System.out.println(" " +a.raiz());
             preorder(a.hijoIzq());
@@ -89,6 +87,7 @@ public class EjerArboles {
 
     }
     public static void postorder(ABBTDA a) {
+        //imrpime arboles menores y después mayores, empezando por las hojas hasta llegar a raiz
         if (!a.arbolVacio()) {
             postorder(a.hijoIzq());
             postorder(a.hijoDer());
@@ -98,6 +97,7 @@ public class EjerArboles {
     }
 
     public static void inorder(ABBTDA a) {
+        //imprime los elementos ordenados de menor a mayor
         if (!a.arbolVacio()) {
             inorder(a.hijoIzq());
             System.out.println(" " + a.raiz());
--- Practica_Cursada/src/test.java
+++ Practica_Cursada/src/test.java
@@ -4,19 +4,6 @@ import impl.ABB;
 
 public class test {
     public static void main(String[] args) {
-        ABBTDA arbol = new ABB(); arbol.inicializarArbol();
-        arbol.agregarElem(100);
-        arbol.agregarElem(200);
-        arbol.agregarElem(50);
-        arbol.agregarElem(10);
-        arbol.agregarElem(60);
-        arbol.agregarElem(250);
-        System.out.println("raiz: " + arbol.raiz() +", Hijo izquierdo: " + arbol.hijoIzq().raiz() + ", Hijo derecho: " + arbol.hijoDer().raiz());
-        System.out.println("\nImprimir ordenados de menor a mayor");
-        EjerArboles.preorder(arbol);
 
-        System.out.println("La cantidad de elementos es: " + EjerArboles.cantidadElementos(arbol));
-        System.out.println("La cantidad de hojas es: " + EjerArboles.contarHojas(arbol));
-        System.out.println("La cantidad de hojas es: " + EjerArboles.contarHojas1(arbol));
     }
 }
